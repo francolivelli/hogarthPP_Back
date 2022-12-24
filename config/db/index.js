966ab080e5529d8b4be37070207bcf8881077d6b
nataliamachella <nataliamachella@gmail.com>
2022-12-24 07:11:41 -0300

    configuración para deploy

--- config/db/index.js
+++ config/db/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-const db = new Sequelize("revista", null, null, {
-  host: "localhost",
+const db = new Sequelize("revista", "revista_user", null, {
+  host: "dpg-cejco1la4991ihmh14tg-a",
   dialect: "postgres",
   logging: false,
 });
